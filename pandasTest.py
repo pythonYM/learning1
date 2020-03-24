fc078a49311c20e4049806cdca1094f18218bd7c
yoshioka2020 <62333967+yoshioka2020@users.noreply.github.com>
2020-03-24 20:56:09 +0900

    20200320

--- pandasTest.py
+++ pandasTest.py
@@ -1,9 +1,14 @@
-import requests
-import pandas as pd
 import mplfinance as mpf
+import pandas as pd
+
+import requests
+
+
+
+
 
 res = requests.get('https://api.cryptowat.ch/markets/bitflyer/btcfxjpy/ohlc')
-data = res.json()['result']['300']  # 15分足を使用する
+data = res.json()['result']['60']  # 60分足を使用する
 
 
 
@@ -12,7 +17,7 @@ df = pd.DataFrame(data, columns=col)
 df = df.set_index('time')  # time列をindexにする
 df.index = pd.to_datetime(df.index, unit='s')  # unixtimeからdatetimeに変換する
 
-mpf.plot(df, type='candle', volume=True, mav=(3,6,9), style='yahoo')
+mpf.plot(df, type='candle', volume=True, mav=(3,6,9))
 
 
 
